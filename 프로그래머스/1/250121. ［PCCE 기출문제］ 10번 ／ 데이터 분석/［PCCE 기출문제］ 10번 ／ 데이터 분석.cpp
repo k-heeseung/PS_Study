9b6807263dcfdd48e36a68b4c4c0ee81a86c07f1
k-heeseung <159872049+k-heeseung@users.noreply.github.com>
2026-08-31 19:01:23 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.40 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
@@ -1,29 +1,29 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) {
     vector<vector<int>> answer;
-    map<string, int> m;
+    unordered_map<string, int> m;
     m["code"] = 0;
     m["date"] = 1;
     m["maximum"] = 2;
     m["remain"] = 3;
     int data_len = data.size();
     int ext_idx = m[ext];
-    for (int i=0; i<data_len; i++){
-        if (data[i][ext_idx] < val_ext){
-            answer.push_back(data[i]);
-        }
-    }
+    auto it = remove_if(data.begin(), data.end(), [ext_idx, val_ext](auto &row){
+        return (row[ext_idx] == val_ext) || (row[ext_idx] > val_ext);
+    });
+    data.erase(it, data.end());
+    
     int sort_col = m[sort_by];
-    sort(answer.begin(), answer.end(), [sort_col](const vector<int> &v1, vector<int> &v2){
+    sort(data.begin(), data.end(), [sort_col](const vector<int> &v1, vector<int> &v2){
         return v1[sort_col] < v2[sort_col];
     });
     
     
-    return answer;
+    return data;
 }
