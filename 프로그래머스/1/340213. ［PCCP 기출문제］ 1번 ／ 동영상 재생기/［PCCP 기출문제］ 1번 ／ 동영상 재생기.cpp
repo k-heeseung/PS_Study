97b1595bb2b400881c3b0a30d8edf51be1431ec3
k-heeseung <159872049+k-heeseung@users.noreply.github.com>
2026-09-01 23:29:52 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cpp
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cpp
@@ -3,24 +3,15 @@
 
 using namespace std;
 
+int to_time (string str){
+    return stoi(str.substr(0, 2)) * 60 + stoi(str.substr(3));
+}
+
 string solution(string video_len, string pos, string op_start, string op_end, vector<string> commands) {
-    string answer = "";
-    int video_len_idx = video_len.find(":");
-    int video_len_m = stoi(video_len.substr(0, video_len_idx));
-    int video_len_s = stoi(video_len.substr(video_len_idx+1));
-    int pos_idx = pos.find(":");
-    int pos_m = stoi(pos.substr(0, pos_idx));
-    int pos_s = stoi(pos.substr(pos_idx+1));
-    int op_s_idx = op_start.find(":");
-    int op_s_m = stoi(op_start.substr(0, op_s_idx));
-    int op_s_s = stoi(op_start.substr(op_s_idx+1));
-    int op_e_idx = op_end.find(":");
-    int op_e_m = stoi(op_end.substr(0, op_e_idx));
-    int op_e_s = stoi(op_end.substr(op_e_idx+1));
-    int video_len_ = (video_len_m)*60 + video_len_s;
-    int pos_ = (pos_m)*60 + pos_s;
-    int op_s_ = (op_s_m)*60 + op_s_s;
-    int op_e_ = (op_e_m)*60 + op_e_s;
+    int video_len_ = to_time(video_len);
+    int pos_ = to_time(pos);
+    int op_s_ = to_time(op_start);
+    int op_e_ = to_time(op_end);
     
     for (auto c : commands){
         // 오프닝 건너뛰기
@@ -38,13 +29,13 @@ string solution(string video_len, string pos, string op_start, string op_end, ve
     }
     
     string answer_m, answer_s;
-    pos_m = pos_/60;
-    pos_s = pos_%60;
+    int pos_m = pos_/60;
+    int pos_s = pos_%60;
     if (pos_m < 10) answer_m = "0" + to_string(pos_m);
     else answer_m = to_string(pos_m);
     if (pos_s < 10) answer_s = "0" + to_string(pos_s);
     else answer_s = to_string(pos_s);
-    answer = answer_m + ":" + answer_s;
+    string answer = answer_m + ":" + answer_s;
     
     return answer;
 }
